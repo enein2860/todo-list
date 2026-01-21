6885018bce81262c0e0264bb1104f251ac4ffe9f
enein2860 <dondigidon.nemets@mail.ru>
2026-01-21 19:33:44 +0300

    added tasks saving

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import InputTaskForm from "./InputTaskForm"
 import SearchTaskForm from "./SearchTaskForm"
 import TodoInfo from "./TodoInfo"
@@ -6,21 +6,16 @@ import TodoList from "./TodoList"
 
 
 const Todo = () => {
-  const [tasks, setTasks] = useState([
-    { id: 1, title: 'task 1', isDone: false },
-    { id: 2, title: 'task 2', isDone: false },
-    { id: 3, title: 'task 3', isDone: false },
-    { id: 4, title: 'task 4', isDone: false },
-    { id: 5, title: 'task 5', isDone: false },
-    { id: 6, title: 'task 6', isDone: false },
-    { id: 7, title: 'task 7', isDone: false },
-    { id: 8, title: 'task 8', isDone: false },
-    { id: 9, title: 'task 9', isDone: false },
-    { id: 10, title: 'task 10', isDone: false },
-  ])
-
   const [newTaskTitle, setNewTaskTitle] = useState('')
   const [filter, setFilter] = useState('')
+  const [tasks, setTasks] = useState(() => {
+    return localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
 
   const onDeleteAllTasks = () => {
     setTasks([])
